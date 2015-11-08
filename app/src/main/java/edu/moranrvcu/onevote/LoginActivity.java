a43e8c1a933843e4b9d51d9fd248c2f1db5f8f36
phantompunk <rigo.moran0@gmail.com>
2015-11-07 20:42:31 -0500

    Scanner added

--- app/src/main/java/edu/moranrvcu/onevote/LoginActivity.java
+++ app/src/main/java/edu/moranrvcu/onevote/LoginActivity.java
@@ -1,9 +1,11 @@
 package edu.moranrvcu.onevote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -33,5 +35,10 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, ScannerActivity.class);
+        startActivity(intent);
     }
 }
